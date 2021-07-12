54fdafaa58b798fc28fe417c3e203d8139456890
xwting <x18291232001@163.com>
2021-07-12 16:15:42 +0800

    test

--- IDEA/ITheima_13/src/day01/Test.java
+++ IDEA/ITheima_13/src/day01/Test.java
@@ -1,4 +1,71 @@
 package day01;
 
 public class Test {
+
+}
+class Animal{
+    private String name;
+    private int age;
+    private char sex;
+
+    public Animal() {
+    }
+
+    public Animal(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return sex
+     */
+    public char getSex() {
+        return sex;
+    }
+
+    /**
+     * 设置
+     * @param sex
+     */
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String toString() {
+        return "Animal{name = " + name + ", age = " + age + ", sex = " + sex + "}";
+    }
 }
